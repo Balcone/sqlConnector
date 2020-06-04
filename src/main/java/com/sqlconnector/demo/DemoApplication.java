f658e9cf37177a8624e92d1b4f47d60a9440943b
5444444 <1003555715@qq.com>
2020-06-04 22:22:38 +0800

    add test .json data

--- src/main/java/com/sqlconnector/demo/DemoApplication.java
+++ src/main/java/com/sqlconnector/demo/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.sqlconnector.demo;
 
+import com.sqlconnector.demo.readinjson.Data;
+import com.sqlconnector.demo.readinjson.ReadInJson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(DemoApplication.class, args);
+
+        String path="src/main/resources/data";
+        ArrayList<Data> datas=new ArrayList<>();
+        new ReadInJson().getFiles(path, datas);
     }
+
+
 
 }
